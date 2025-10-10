1c7c6d952c748c73c9bf767410ce01bb8aac1621
toni <canillask8@gmail.com>
2025-10-10 20:16:53 +0200

    feat: complete exercise 1

---
+++ arrow-functions.js
@@ -0,0 +1,10 @@
+/*Exercici 1
+Conversió de funcions:
+ Tens una funció add que accepta dos paràmetres i retorna la seva suma. 
+ Converteix-la en una funció de fletxa. Per exemple: function add(a, b) {return a + b;}.
+ */
+
+ const add = (a, b) => a + b;
+
+ console.log(add(2, 9));
+
