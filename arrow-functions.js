fa7052516600157cefbc562537736b267b8e599b
toni <canillask8@gmail.com>
2025-10-10 20:21:00 +0200

    feat: complete exercise 2

--- arrow-functions.js
+++ arrow-functions.js
@@ -6,5 +6,20 @@ Conversió de funcions:
 
  const add = (a, b) => a + b;
 
- console.log(add(2, 9));
+ //console.log(add(2, 9));
+
+/*
+Exercici 2
+Funció de fletxa sense paràmetres: 
+Crea una funció de fletxa anomenada randomNumber que no necessiti 
+paràmetres i que retorni un número aleatori entre 0 i 100.
+*/
+
+let randomNumber = () => Math.random();
+
+
+
+console.log(randomNumber());
+
+
 
