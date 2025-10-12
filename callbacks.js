9121aaff889de254e1e6c2c418b44cac85377c76
toni <canillask8@gmail.com>
2025-10-12 22:57:36 +0200

    feat: add exercise 2 with math operators in callback

--- callbacks.js
+++ callbacks.js
@@ -15,3 +15,17 @@ function callBack(n) {
     console.log(`The number is ${n}`);    
 }
 process(22, callBack);
+
+/* Exercici 2
+Callbacks amb operacions matemàtiques: 
+Escriu una funció calculadora que accepti tres paràmetres: dos nombres i una funció de callback. 
+La funció calculadora ha d'invocar la funció de callback amb els dos nombres com a paràmetres. 
+Després, crida calculadora amb una funció que faci la suma dels dos nombres. */
+
+function calculator(n1, n2, cb) {
+    cb(n1, n2);
+}
+function numbers(num1, num2) {
+    console.log(`The add of ${num1} and ${num2} is ${num1 + num2}`);
+}
+calculator(5, 9, numbers);
