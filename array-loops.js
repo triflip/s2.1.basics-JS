3654f9d8e2148d91682ad999cc409caf266bdc7d
toni <canillask8@gmail.com>
2025-10-13 20:06:39 +0200

    feat: add exercise 3 using filter to extract even numbers from array

--- array-loops.js
+++ array-loops.js
@@ -13,3 +13,14 @@ let noms = ['Anna', 'Bernat', 'Clara']; */
 for (let nom of noms) {
   console.log(nom);
 }
+
+/* Exercici 3
+filter: Teniu una array de números. Utilitza filter per a crear una nova 
+array que només contingui els números parells. 
+let numeros = [1, 2, 3, 4, 5, 6];
+*/
+
+let numeros = [1, 2, 3, 4, 5, 6];
+let parells = numeros.filter(n => n % 2 === 0);
+
+console.log(parells); 
