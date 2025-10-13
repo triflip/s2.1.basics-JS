632026ef260627de600c4ddecd808bd86bde2a0d
toni <canillask8@gmail.com>
2025-10-13 20:47:28 +0200

    feat: add exercise 3 creating conditional promise with resolve or reject

--- promises&sync-await.js
+++ promises&sync-await.js
@@ -13,3 +13,15 @@ Utilització d'una Promesa: Utilitza la promesa creada en l'exercici anterior.
 Crea un .then que imprimeixi el resultat a la consola. */
 
 promesa.then(resultat => console.log(resultat)); 
+
+/* Exercici 3
+Promesa amb reject: Crea una promesa que es resolgui després de 2 segons si l'input és igual a 'Hola',
+ i que la rebutgi si l'input és qualsevol altra cosa. */
+
+ function saluda(input) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      input === "Hola" ? resolve("Salutació acceptada") : reject("Entrada incorrecta");
+    }, 2000);
+  });
+}
