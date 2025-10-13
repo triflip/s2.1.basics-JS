8f2ef4d71d37a4ac77991af441619f7f153698ef
toni <canillask8@gmail.com>
2025-10-13 19:42:41 +0200

    feat: add exercise 5 using every to validate all values over 10

--- array-transformations.js
+++ array-transformations.js
@@ -1,6 +1,4 @@
 /*
-Nivell 1
-
 Exercici 1
 Map: Teniu un array de números [1, 2, 3, 4]. 
 Crea una nova array que contingui el quadrat de cada número.
@@ -45,20 +43,24 @@ const suma = nums4.reduce((acc, val) => acc + val, 0);
 console.log(suma); 
 
 /*
-Nivell 2
-*/
-/*
 Exercici 5
-Donat un array "[ 1, 3, 7, 10 ,15, 17, 11, 5, 8, 12, 9 ]", crea una funció en una sola línia que faci el següent:
-
+Donat un array "[ 1, 3, 7, 10 ,15, 17, 11, 5, 8, 12, 9 ]", 
+crea una funció en una sola línia que faci el següent:
 - Filtra els nombres majors o iguals a 10.
-
 - Multiplica cada nombre filtrat per 2.
-
 - Calcula la suma dels nombres filtrats i multiplicats per 2.
-
 - La funció ha de retornar el resultat de la suma.
 */
+
+const nums5 = [1, 3, 7, 10, 15, 17, 11, 5, 8, 12, 9];
+const resultat = nums5
+  .filter(n => n >= 10)
+  .map(n => n * 2)
+  .reduce((acc, val) => acc + val, 0);
+  
+console.log(resultat); 
+
+
 /*
 Nivell 3
 Exercici 6
