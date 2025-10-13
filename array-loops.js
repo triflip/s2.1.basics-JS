7aec95ef8cf68df142e371c65c5dd9b27147f580
toni <canillask8@gmail.com>
2025-10-13 20:15:52 +0200

    feat: add exercise 5 using for-of with manual index tracking

--- array-loops.js
+++ array-loops.js
@@ -34,3 +34,15 @@ let obj = { nom: "Ona", edat: 25, ciutat: "Barcelona" };
 for (let clau in obj) {
   console.log(clau, obj[clau]);
 }
+
+/* Exercici 5
+for-of amb break: Teniu una array de números. 
+Utilitza un bucle for-of per a imprimir a la consola els números fins a trobar el número 5, 
+llavors atura el bucle: let numeros = [1, 2, 3, 4, 5, 6]; */
+
+
+let nums = [1, 2, 3, 4, 5, 6];
+for (let num of nums) {
+  if (num === 5) break;
+  console.log(num);
+}
