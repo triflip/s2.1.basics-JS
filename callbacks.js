9094f8628ccd60247477a3c90bd569c6015c39e9
toni <canillask8@gmail.com>
2025-10-13 12:16:34 +0200

    feat: add exercise 5 with string modification via callback

--- callbacks.js
+++ callbacks.js
@@ -68,3 +68,21 @@ function loop(array) {
     }
 }
 processElements(elements, loop);
+
+/* Nivell 3
+Exercici 5
+Escriu una funció processarCadena que accepti dos paràmetres: 
+una cadena de caràcters i una funció de callback. La funció processarCadena ha de convertir 
+la cadena a majúscules i llavors invocar la funció de callback amb la cadena transformada. */
+
+const charString = "ljtmp";
+
+function processarCadena(stg, cb) {
+   let toUpoer = stg.toUpperCase();
+    cb(toUpoer);
+}
+
+function toUpperC(result) {
+    console.log(result);
+}
+processarCadena(charString, toUpperC);
