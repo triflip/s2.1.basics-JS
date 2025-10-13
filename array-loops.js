be3a71056210a2f1f398f474b5fee6594ac3ac30
toni <canillask8@gmail.com>
2025-10-13 19:53:13 +0200

    feat: add exercise 1 using forEach to print array elements

---
+++ array-loops.js
@@ -0,0 +1,7 @@
+/* Exercici 1
+forEach: Teniu una array de noms. Utilitza forEach per a imprimir cada nom a la consola: 
+let noms = ['Anna', 'Bernat', 'Clara']; */
+
+let noms = ['Anna', 'Bernat', 'Clara'];
+
+noms.forEach(nom => console.log(nom));
