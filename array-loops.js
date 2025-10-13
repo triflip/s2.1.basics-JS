d433dfe1dd8fee990836a47b43e05ee21af3ae64
toni <canillask8@gmail.com>
2025-10-13 19:55:15 +0200

    feat: add exercise 2 using for-of loop to iterate over names

--- array-loops.js
+++ array-loops.js
@@ -5,3 +5,11 @@ let noms = ['Anna', 'Bernat', 'Clara']; */
 let noms = ['Anna', 'Bernat', 'Clara'];
 
 noms.forEach(nom => console.log(nom));
+
+/* Exercici 2
+for-of: Teniu una array de noms. Utilitza un bucle for-of per a imprimir cada nom a la consola: 
+let noms = ['Anna', 'Bernat', 'Clara']; */
+
+for (let nom of noms) {
+  console.log(nom);
+}
