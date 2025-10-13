a04161a95a51272efafb2e1baed30861df467a03
toni <canillask8@gmail.com>
2025-10-13 19:33:42 +0200

    feat: add exercise 2 using filter to select even numbers

--- array-transformations.js
+++ array-transformations.js
@@ -6,17 +6,25 @@ Map: Teniu un array de números [1, 2, 3, 4].
 Crea una nova array que contingui el quadrat de cada número.
 */
 
-const nums = [1, 2, 3, 4];
-const quadrats = nums.map(n => n * n);
+const nums1 = [1, 2, 3, 4];
+const quadrats = nums1.map(n => n * n);
 
 console.log(quadrats);
  
-
 /*
 Exercici 2
-Filter: Teniu una array de números [1, 2, 3, 4]. Crea una nova array que només contingui els números parells.
+Filter: Teniu una array de números [1, 2, 3, 4]. 
+Crea una nova array que només contingui els números parells.
 */
+
+const nums2 = [1, 2, 3, 4];
+const parells = nums2.filter(n=> n % 2 === 0);
+
+console.log(parells);
+
+
 /*
+
 Exercici 3
 Find: Teniu una array de números [1, 10 , 8, 11]. Utilitza la funció find per a trobar el primer número que és major a 10.
 */
